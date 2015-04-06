d853b01c7c5834f041468e2d6ee64f15c53ae73e
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-06 19:33:13 +0200

    add the serialization

--- Patterns/memorization.js
+++ Patterns/memorization.js
@@ -12,3 +12,20 @@ var myFunc = function(param) {
 };
 // cache storage
 myFunc.cache = {};
+
+/**
+ * If you want to serialized the result, for  example in JSON format
+ !! The object identifier is lost!!
+ */
+var myFunc = function() {
+    var cachekey = JSON.stringify(Array.prototype.slice.call(arguments)),
+      result;
+    if (!myFunc.cache[cachekey]) {
+      result = {};
+      // ... expensive operation ...
+      myFunc.cache[cachekey] = result;
+    }
+    return myFunc.cache[cachekey];
+  }
+  // cache storage
+myFunc.cache = {};
