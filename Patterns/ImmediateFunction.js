031c86e374b4bbb82374aa7b194d275b01342f6e
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-06 19:11:11 +0200

    new comment  into the code

--- Patterns/ImmediateFunction.js
+++ Patterns/ImmediateFunction.js
@@ -1,6 +1,7 @@
 /**
 *   Immediate function pattern  to execute it as soon as it is defined
 *   This pattern is useful because it provide a scope sandbox for your initialization code
+*   It protect the global scope pollution
 */
 (function() {
   alert('watch out!');
