ef3fb5449bcff2559f9d5cafdc0d99a5e6cd000f
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-07 12:50:20 +0200

    namespace pattern

---
+++ Patterns/namespace.js
@@ -0,0 +1,22 @@
+/**
+* This pattern (and relate function) permit to declare namespaces in your application for your modules
+* and permit to not redeclare the same module
+*/
+var MYAPP = MYAPP || {};
+MYAPP.namespace = function(ns_string) {
+  var parts = ns_string.split('.'),
+    parent = MYAPP,
+    i;
+  // strip redundant leading global
+  if (parts[0] === "MYAPP") {
+    parts = parts.slice(1);
+  }
+  for (i = 0; i < parts.length; i += 1) {
+    // create a property if it doesn't exist
+    if (typeof parent[parts[i]] === "undefined") {
+      parent[parts[i]] = {};
+    }
+    parent = parent[parts[i]];
+  }
+  return parent;
+}
