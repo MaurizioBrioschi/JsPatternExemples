7a542b9b5cd1453471a45fe21107419a92c223f5
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-08 11:54:10 +0200

    refactoring of the module pattern

--- Patterns/module.js
+++ Patterns/module.js
@@ -1,41 +1,33 @@
-var ridesoft = ridesoft || {};
-
-    (function ( ridesoft) {
-        ridesoft.module = (function() {
-            var module = {};
-            module.info = {
-                name: "Base Module",
-                version : "1.0",
-                author: "Maurizio Brioschi"
-                
-            };
-
-            module.showInfo = function() {
-                var result = "";
-                for (key in this.info) {
-                    result += key + ": " + this.info[key] + "<br>"
-                }
-                return(result);
-            }
-
-            module.log = function(data) {
-                console.log(data);
-            };
-
-            module.extend = function() {
-                var sonModule = {};
-                components = [this].concat([].slice.call(arguments));
-                for (var i=0; i<components.length; i++) {
-                    for (key in components[i]) {
-                        sonModule[key] = components[i][key];
-                    }
-                }
-                return sonModule;
-            };
-
-            module.init = function() {};
-
-            return module;
-        })();
-
-    }(ridesoft));
+/**
+*   Very useful to organize your appplication when its grown
+*   Every module must work separately
+*   It use the namespace pattern, js not have its native namespace declaration
+*/
+MYAPP.namespace('MYAPP.utilities.array');
+MYAPP.utilities.array = (function() {
+  // dependencies
+  var uobj = MYAPP.utilities.object,
+    ulang = MYAPP.utilities.lang,
+    // private properties
+    array_string = "[object Array]",
+    ops = Object.prototype.toString;
+  // private methods
+  // ...
+  // end var
+  // optionally one-time init procedures
+  // ...
+  // public API
+  return {
+    inArray: function(needle, haystack) {
+      for (var i = 0, max = haystack.length; i < max; i += 1) {
+        if (haystack[i] === needle) {
+          return true;
+        }
+      }
+    },
+    isArray: function(a) {
+        return ops.call(a) === array_string;
+      }
+      // ... more methods and properties
+  };
+}());
