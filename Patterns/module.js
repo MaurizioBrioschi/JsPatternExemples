b60b0227e5d6b14dc8b7a6e645fb8dae61d4b35b
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-08 12:04:19 +0200

    module pattern that produce construnctor

--- Patterns/module.js
+++ Patterns/module.js
@@ -1,8 +1,8 @@
 /**
-*   Very useful to organize your appplication when its grown
-*   Every module must work separately
-*   It use the namespace pattern, js not have its native namespace declaration
-*/
+ *   Very useful to organize your appplication when its grown
+ *   Every module must work separately
+ *   It use the namespace pattern, js not have its native namespace declaration
+ */
 MYAPP.namespace('MYAPP.utilities.array');
 MYAPP.utilities.array = (function() {
   // dependencies
@@ -31,3 +31,40 @@ MYAPP.utilities.array = (function() {
       // ... more methods and properties
   };
 }());
+
+/**
+ *   In this way you can create e  module that produce construnctor
+ *   to isntanciate an object
+ */
+MYAPP.namespace('MYAPP.utilities.Array');
+MYAPP.utilities.Array = (function() {
+  // dependencies
+  var uobj = MYAPP.utilities.object,
+    ulang = MYAPP.utilities.lang,
+    // private properties and methods...
+    Constr;
+  // end var
+  // optionally one-time init procedures
+  // ...
+  // public API -- constructor
+  Constr = function(o) {
+    this.elements = this.toArray(o);
+  };
+  // public API -- prototype
+  Constr.prototype = {
+    constructor: MYAPP.utilities.Array,
+    version: "2.0",
+    toArray: function(obj) {
+      for (var i = 0, a = [], len = obj.length; i < len; i += 1) {
+        a[i] = obj[i];
+      }
+      return a;
+    }
+  };
+  // return the constructor
+  // to be assigned to the new namespace
+  return Constr;
+}());
+
+//you can use in this way
+var arr = new MYAPP.utilities.Array(obj);
